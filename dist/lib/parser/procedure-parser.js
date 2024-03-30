5d3f3c4dc8989ca525d5981d3972bd1783e89abf
Koen Vanhoof <vanhoof.koen@gmail.com>
2024-03-30 17:21:26 +0100

    improved parsing of functions returning package referenced types

--- dist/lib/parser/procedure-parser.js
+++ dist/lib/parser/procedure-parser.js
@@ -38,34 +38,52 @@ class ProcedureParser extends process_like_parse_1.ProcessLikeParser {
         }
         if (this.is_function){
             this.expect("return")
-            const type = this.getNextWord()
+          //const type = this.advanceSemicolon()
+          //skip all the following which is the type definition. 
+          // this can be a single word or a lib.package.type case.
+          // anyway it is delimited by a space.
+          // after the space there is either a ; or a is
+          const type = this.getNextWord()
         }
-
-        let nextWord = this.getNextWord({ consume: false });
-        procedure.range.end.i = this.pos.i;
-        if (nextWord.toLowerCase() === 'is') {
-            this.expect('is');
-            // debugger;
-            nextWord = this.getNextWord({ consume: false }).toLowerCase();
-            new declarative_part_parser_1.DeclarativePartParser(this.text, this.pos, this.file, procedure).parse();
-
-            this.expect('begin');
-            procedure.statements = this.parseStatements(procedure, ['end']);
-            this.expect('end');
-
-            if (this.is_function){
-                this.maybeWord('function');
-            }
-            else{
-                this.maybeWord('procedure');
-            }
-            //console.log("name = "+procedure.name.text)
-            this.maybeWord(procedure.name.text);
-            procedure.definition = this.parent;
+        //look for the position of CR
+        let lastCharIndex = this.text.substr(this.pos.i, this.text.length).search(/\n/)
+        //check if there is a ; on the current line
+        let semicolPos = this.text.substr(this.pos.i, lastCharIndex).search(";")
+        if (semicolPos < 0) {
+            this.advancePast(/\s/)
+            let nextWord = this.getNextWord({ consume: false });
+            procedure.range.end.i = this.pos.i;
+            if (nextWord.toLowerCase() === 'is') {
+                this.expect('is');
+                // debugger;
+                nextWord = this.getNextWord({ consume: false }).toLowerCase();
+                new declarative_part_parser_1.DeclarativePartParser(this.text, this.pos, this.file, procedure).parse();
+    
+                this.expect('begin');
+                procedure.statements = this.parseStatements(procedure, ['end']);
+                this.expect('end');
+    
+                if (this.is_function){
+                    this.maybeWord('function');
+                }
+                else{
+                    this.maybeWord('procedure');
+                }
+                //console.log("name = "+procedure.name.text)
+                this.maybeWord(procedure.name.text);
+                procedure.definition = this.parent;
+            }    
+        }
+        else {
+            //rather than this.expect(";") to support return values like dt.package.type
+            // at this point, we read the first word after return and it is not "is"
+            // if a package referenced type is returned, a series of .string combinations can follow
+            // but in the end we need to find a semicolon.
+            this.advanceSemicolon() 
+            //this.expect(';');
+            //end.line -=1
         }
         const end = new objects_1.OI(this.parent, this.pos.i)
-        this.expect(';');
-        //end.line -=1
         let start = new objects_1.OI(this.parent, procedure.range.start.i)
         procedure.range = new objects_1.OIRange(this.parent, start, end)
         return procedure;
